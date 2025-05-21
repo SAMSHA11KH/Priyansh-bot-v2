ca0048debe6ea7274cff715e5e7e7f5c84f9d51e
Sam Shaikh <samrshaikh70@gmail.com>
2025-05-21 22:56:47 +0300

    Update 0_SAMEER COLA-DUDH.js

--- Priyansh/commands/0_SAMEER COLA-DUDH.js
+++ Priyansh/commands/0_SAMEER COLA-DUDH.js
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("cola")==0 || event.body.indexOf("COLA")==0 || event.body.indexOf("Cola")==0 || event.body.indexOf("Coka cola")==0) {
                 var msg = {
-                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐒𝐇𝐀𝐀𝐍 𝐊𝐇𝐀𝐍 𝐊 \n< ────────────────── >\n\n𝐘𝐞 𝐋𝐨 𝐉𝐚𝐍𝐞𝐌𝐚𝐍 𝐂𝐨𝐋𝐚 𝐏𝐢𝐘𝐎 \n< ────────────────── >",
+                                body: "=𝐎𝐰𝐧𝐞𝐫 ➻  𝐒𝐀𝐦𝐞𝐞𝐫 𝐊𝐡𝐀𝐚𝐧 \n< ────────────────── >\n\n𝐘𝐞 𝐋𝐨 𝐉𝐚𝐍𝐞𝐌𝐚𝐍 𝐂𝐨𝐋𝐚 𝐏𝐢𝐘𝐎 \n< ────────────────── >",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/94bb49af864867ba223903adba404e16.jpg`)
                         }
                         api.sendMessage(msg, threadID, messageID);
